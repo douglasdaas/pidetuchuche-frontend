d203552013baed3db2c56190a005e9ef4249fd07
douglasdaas <douglasdaas@gmail.com>
2020-03-31 22:27:07 -0400

    Almost done

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ console.log('hola');
 
 
 
-
 let resultado = document.getElementById('container-product');
 let sel = document.getElementById('product_category')
 let sel1 = document.getElementById('product_category1')
-let pContainer = document.getElementById('container-products')
+let pContainer = document.getElementById('container-products');
+let createProductContainer = document.getElementById('createProductContainer');
 const url = 'https://pidetuchuche-backend.herokuapp.com';
 const req = new XMLHttpRequest();
 
@@ -18,6 +18,30 @@ let scroll = document.getElementById('scrollButton');
 function scrollHeight() {
   window.scrollBy(0, height);
 }
+
+
+
+//filter
+function filter() {
+  console.log('epaoeoe');
+  let searchInput = document.getElementById('search');
+  let filterValue = searchInput.value.toUpperCase();
+  let productList = document.getElementById('container-products');
+  let element = productList.getElementsByClassName('wrapper')
+  console.log(element);
+  for (let i = 0; i < element.length; i++) {
+    let a = element[i].getElementsByTagName('h1')[0];
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1 ) {
+      element[i].style.display = "";
+    }else {
+      element[i].style.display = "none";
+    }
+  }
+}
+
+
+
+
 //GET ALL
 function getProducts() {
   req.open('GET', url + '/productos', true);
@@ -29,15 +53,14 @@ function getProducts() {
         console.log(products);
         for (let i = 0; i < products.datos.length; i++) {
           let div = document.createElement('div')
-          div.innerHTML = '<div data-aos="flip-up" class="wrapper cards"> <div class="row"> <div class="product-info col"><div class="product-text"><h1>' + products.datos[i].nombre + '</h1><h2>' + products.datos[i].categorias + '</h2><h3>Precio: ' + products.datos[i].precio  + '$</h3><h4>Stock: ' + products.datos[i].cantidad + '</h4><p>' + products.datos[i].descripcion + '</p><div class="product-data"><button type="button" name="button">Compra</button></div></div></div><div class="product-img col"><img id="productImage" style="background-image: url(' + products.datos[i].ruta_imagen +  ');background-size: cover;background-position: center;height: 320px;" class="img-fluid img-container"></div></div></div>';
+          div.innerHTML = '<div class="wrapper cards"> <div class="row"> <div class="product-info col"><div class="product-text"><h1>' + products.datos[i].nombre + '</h1><h2>' + products.datos[i].categorias + '</h2><h3>Precio: ' + products.datos[i].precio  + '$</h3><h4>Stock: ' + products.datos[i].cantidad + '</h4><p>' + products.datos[i].descripcion + '</p><div class="product-data"><button data-toggle="modal" data-target="#buyInfo" type="button" name="button">Compra</button></div></div></div><div class="product-img col"><img id="productImage" style="background-image: url(' + products.datos[i].ruta_imagen +  ');background-size: cover;background-position: center;height: 320px;" class="img-fluid img-container"></div></div></div>';
           pContainer.appendChild(div);
         }
       }
-    } style="background-image: url();background-size: cover;background-position: center;height: 320px;"
+    }
   };
   req.send();
 }
-
 
 
 
@@ -67,11 +90,11 @@ function getCategorias() {
 
 
 
- window.onload = function() {
+window.onload = function() {
   scroll.addEventListener("click", scrollHeight);
   getCategorias();
   getProducts();
- };
+};
 
 
 function printTok() {
@@ -119,26 +142,24 @@ function createProduct() {
   let productoJSON = JSON.stringify(producto,2,2);
 
   req.open("POST", url + '/productos', true);
-
-  // req.setRequestHeader("Content-type", "multipart/form-data");
-
   req.setRequestHeader("Authorization", tokenCapitalized);
+
+
   console.log('productoJSON:: ',productoJSON);
-  console.log('productData:: ',productData)
+  console.log('productData:: ',productData);
   req.send(productData);
+  alert('Producto creado');
 
 }
 
 //POST LOGIN
 function loginOnClick() {
-
   let inicio = function(callback) {
     if (typeof callback === 'function') {
-      callback()
-      alert( localStorage.getItem('authToken') );
-    }else {
-      console.log('no se pudo joto');
-      setTimeout( inicio, 500 );
+      callback();
+    }
+    else {
+      alert('Algo salio mal');
     }
   }
 
@@ -147,20 +168,20 @@ function loginOnClick() {
     req.open("POST", url + '/login', true);
     req.setRequestHeader("Content-type", "application/json");
     req.onreadystatechange = function wait() {
-      if (req.readyState === 4 && req.status === 200) {
-        let session = JSON.parse(this.responseText);
+      if (req.readyState === 4 && req.status === 200 ) {
+        session = JSON.parse(this.responseText);
         let sessionToken = session.data;
         let authToken = sessionToken.type + ' ' + sessionToken.token;
         localStorage.setItem('authToken', authToken );
-        console.log(session);
-        console.log(authToken);
+        createProductContainer.innerHTML = '<button data-dismiss="modal" type="button" class="btn btn-primary" data-toggle="modal" data-target="#createProduct">Crear producto</button>'
+        alert('Bienvenido');
+      }else if (req.status === 400) {
+        alert('Teequivocaste');
       }
     };
-    let data = JSON.stringify({ "email": 'admin@pidetuchuche.com', "password": '12345' });
+    let user = document.getElementById('email').value;
+    let pass = document.getElementById('password').value;
+    let data = JSON.stringify({ "email": user, "password": pass });
     req.send(data);
   })
 }
-
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
